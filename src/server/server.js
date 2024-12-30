413dfcb7a896bc7813197e8c62d0dfa3cc499bce
Juan Pablo Fernandez <juampifer@gmail.com>
2024-12-30 12:42:35 -0300

    Instalar y configurar CORS

--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
-// const router = require('../routes/routes');
+const cors = require('cors');
 const productRoutes = require('../routes/product.routes');
 
 // Configurar el servidor
 const server = express();
+
+// Configurar CORS para permitir solicitudes desde el frontend
+server.use(cors({
+    origin: 'http://localhost:3000', // Reemplaza con el dominio de tu frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
+}));
 
 // Le permite a express entender el json del body de las peticiones
 server.use(express.json())
